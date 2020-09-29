2d194925acd1a45e84e3b6221ad0f55545117995
Henry Zheng <henryzheng@g.ucla.edu>
2020-09-29 11:39:03 -0700

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 19:35:14 2020
+
+@author: hwz62
+"""
+import pandas as pd
+from math import log, exp
+from raICD import icd9, icd10
+from random import choices
+from random import shuffle
+
+
+def containsICD(icdList):
+    icdCount = 0
+    for code in icd9:
+        if code in icdList:
+            icdCount += 1
+        else:
+            continue
+    for c in icd10:
+        if any(s.startswith(c) for s in icdList):
+            icdCount += 1
+        else:
+            continue
+    return icdCount
+
+
+def countAllICD(mrn, problemList, diagnoses):
+    plICD = list(problemList[problemList['MRN'] == mrn]['ICD_CODE'])
+    dxICD = list(diagnoses[diagnoses['MRN'] == mrn]['ICD_CODE'])
+    totalCounts = containsICD(plICD)+containsICD(dxICD)
+    return totalCounts
+
+
+def countDXICD(mrn, diagnoses):
+    dxICD = list(diagnoses[diagnoses['MRN'] == mrn]['ICD_CODE'])
+    totalCounts = containsICD(dxICD)
+    return totalCounts
+
+
+def harvard(mrn, demographics, diagnoses):
+    ICDcounts = countDXICD(mrn, diagnoses)
+    lupus = demographics[demographics['MRN']==mrn]['LUPUS_CNT'].notna().bool()
+    if lupus == True:
+        lupusCt = int(demographics[demographics['MRN']==mrn]['LUPUS_CNT'])
+    else:
+        lupusCt = 0
+    psoriatic = demographics[demographics['MRN']==mrn]['PSORIATICARTHRO_CNT'].notna().bool()
+    if psoriatic == True:
+        psoriaticCt = int(demographics[demographics['MRN']==mrn]['PSORIATICARTHRO_CNT'])
+    else:
+        psoriaticCt = 0
+    lab = demographics[demographics['MRN']==mrn]['LAB_RF'].notna().bool()
+    if (lab == True) and (float(demographics[demographics['MRN']==mrn]['LAB_RF']) > 0):
+        labCt = 1
+    else:
+        labCt = 0
+    dxent = demographics[demographics['MRN']==mrn]['PATIENT_DXENCT'].notna().bool()
+    if dxent == True:
+        dxentCt = int(demographics[demographics['MRN']==mrn]['PATIENT_DXENCT'])
+    else:
+        dxentCt = 0
+    sum_beta = 1.937*log(1+ICDcounts)-0.529*log(1+lupusCt)-0.122*log(1+psoriaticCt)+1.639*labCt-0.954*log(1+dxentCt)
+    value = exp(-1.017 + sum_beta)/(1 + exp(-1.017 + sum_beta))
+    return value
+
+
+def ageBin(i):
+    if i <= 27:
+        return '18-27'
+    elif (i >= 28 and i <= 37):
+        return '28-37'
+    elif (i >= 38 and i <= 47):
+        return '38-47'
+    elif (i >= 48 and i <= 57):
+        return '48-57'
+    elif (i >= 58 and i <= 67):
+        return '58-67'
+    elif (i >= 68 and i <= 77):
+        return '68-77'
+    elif (i >= 78 and i <= 87):
+        return '78-87'
+    elif i >= 88:
+        return '88+'
+    else:
+        return ValueError('Unfound age decile {}'.format(i))
+
+def ethnicityBin(j):
+    if j == 'Not Hispanic or Latino':
+        return 'Not Hispanic'
+    elif (j == 'Unknown' or j == 'Patient Refused'):
+        return 'Unknown/Patient Refused'
+    else:
+        return 'Hispanic'
+
+def raceBin(k):
+    if k == 'White or Caucasian':
+        return 'White'
+    elif (k == 'Unknown' or k == 'Patient Refused'):
+        return 'Unknown/Patient Refused'
+    else:
+        return 'Not White'
+
+def encoder(sex, race, eth, age):
+    code=['9', '9', '9', '9']
+    if sex=='Female':
+        code[0]='a'
+    if sex=='Male':
+        code[0]='b'
+    if sex=='Unknown':
+        code[0]='c'
+    if race=='White':
+        code[1]='a'
+    if race=='Not White':
+        code[1]='b'
+    if race=='Unknown/Patient Refused':
+        code[1]='c'
+    if eth=='Not Hispanic':
+        code[2]='a'
+    if eth=='Hispanic':
+        code[2]='b'
+    if eth=='Unknown/Patient Refused':
+        code[2]='c'
+    if age=='18-27':
+        code[3]='a'
+    if age=='28-37':
+        code[3]='b'
+    if age=='38-47':
+        code[3]='c'
+    if age=='48-57':
+        code[3]='d'
+    if age=='58-67':
+        code[3]='e'
+    if age=='68-77':
+        code[3]='f'
+    if age=='78-87':
+        code[3]='g'
+    if age=='88+':
+        code[3]='h'
+    return ''.join(code)
+
+def numPatientTypes(num, patients):
+    codeDist = dict(patients['CODE'].value_counts())
+    weights = list(codeDist.values())
+    population = list(codeDist.keys())
+    return choices(population=population,weights=weights, k=num)
+
+
+def produceCohort(ptct, drawingDf, patients):
+    codesList = numPatientTypes(ptct, drawingDf)
+    cohort = []
+    for code in codesList:
+        MRNSubset = list(drawingDf[drawingDf['CODE']==code]['MRN'])
+        shuffle(MRNSubset)
+        MRNPull = MRNSubset[0]
+        cohort.append(MRNPull)
+        MRNIndex = drawingDf.loc[drawingDf['MRN']==MRNPull].index[0]
+        drawingDf = drawingDf.drop([MRNIndex])
+    cohortDf = pd.DataFrame()
+    for mrn in cohort:
+        row=patients.loc[patients['MRN']==mrn]
+        cohortDf = cohortDf.append(row)
+    return cohortDf, drawingDf
---
+++ loadlists.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 27 17:17:55 2020
+
+@author: hwz62
+"""
+
+import pandas as pd
+from random import shuffle
+
+set3Filename = 'set3_single235_09252020.xlsx'
+set3LucasFilename = 'set3_single235_lucas_09252020.xlsx'
+set2IAA = 'set2_iaa50_09252020.xlsx'
+
+cohort1 = pd.read_excel(set3Filename, usecols='A:J', index_col=0, sheet_name='Cohort 1')
+cohort2 = pd.read_excel(set3Filename, usecols='A:J', index_col=0, sheet_name='Cohort 2')
+cohort3 = pd.read_excel(set3Filename, usecols='A:J', index_col=0, sheet_name='Cohort 3')
+cohort4 = pd.read_excel(set3Filename, usecols='A:J', index_col=0, sheet_name='Cohort 4')
+cohort5 = pd.read_excel(set3Filename, usecols='A:J', index_col=0, sheet_name='Cohort 5')
+cohort6 = pd.read_excel(set3Filename, usecols='A:J', index_col=0, sheet_name='Cohort 6')
+cohort7 = pd.read_excel(set3Filename, usecols='A:J', index_col=0, sheet_name='Cohort 7')
+cohort8 = pd.read_excel(set3Filename, usecols='A:J', index_col=0, sheet_name='Cohort 8')
+cohortLucas = pd.read_excel(set3LucasFilename, usecols='A:J', index_col=0).sort_index()
+iaa = pd.read_excel(set2IAA, usecols='A:J', index_col=0)
+
+cohort1Index = list(cohort1.index)
+cohort2Index = list(cohort2.index)
+cohort3Index = list(cohort3.index)
+cohort4Index = list(cohort4.index)
+cohort5Index = list(cohort5.index)
+cohort6Index = list(cohort6.index)
+cohort7Index = list(cohort7.index)
+cohort8Index = list(cohort8.index)
+cohortLucasIndex = list(cohortLucas.index)
+iaaIndex = list(iaa.index)
+
+shuffle(cohort1Index)
+shuffle(cohort2Index)
+shuffle(cohort3Index)
+shuffle(cohort4Index)
+shuffle(cohort5Index)
+shuffle(cohort6Index)
+shuffle(cohort7Index)
+shuffle(cohort8Index)
+shuffle(cohortLucasIndex)
+shuffle(iaaIndex)
+
+IndexOrder = list(range(285))
+shuffle(IndexOrder)
+iaaPos = IndexOrder[:50]
+indPos = IndexOrder[50:]
+
+newCohort1 = pd.DataFrame()
+newCohort2 = pd.DataFrame()
+newCohort3 = pd.DataFrame()
+newCohort4 = pd.DataFrame()
+newCohort5 = pd.DataFrame()
+newCohort6 = pd.DataFrame()
+newCohort7 = pd.DataFrame()
+newCohort8 = pd.DataFrame()
+newCohortLucas = pd.DataFrame()
+
+for i in range(len(indPos)):
+    pos = indPos[i]
+    newCohort1[pos]=cohort1.loc[cohort1Index[i]]
+    newCohort2[pos]=cohort2.loc[cohort2Index[i]]
+    newCohort3[pos]=cohort3.loc[cohort3Index[i]]
+    newCohort4[pos]=cohort4.loc[cohort4Index[i]]
+    newCohort5[pos]=cohort5.loc[cohort5Index[i]]
+    newCohort6[pos]=cohort6.loc[cohort6Index[i]]
+    newCohort7[pos]=cohort7.loc[cohort7Index[i]]
+    newCohort8[pos]=cohort8.loc[cohort8Index[i]]
+    newCohortLucas[pos]=cohortLucas.loc[cohortLucasIndex[i]]
+
+for j in range(len(iaaPos)):
+    pos = iaaPos[j]
+    newCohort1[pos]=iaa.loc[iaaIndex[j]]
+    newCohort2[pos]=iaa.loc[iaaIndex[j]]
+    newCohort3[pos]=iaa.loc[iaaIndex[j]]
+    newCohort4[pos]=iaa.loc[iaaIndex[j]]
+    newCohort5[pos]=iaa.loc[iaaIndex[j]]
+    newCohort6[pos]=iaa.loc[iaaIndex[j]]
+    newCohort7[pos]=iaa.loc[iaaIndex[j]]
+    newCohort8[pos]=iaa.loc[iaaIndex[j]]
+    newCohortLucas[pos]=iaa.loc[iaaIndex[j]]
+
+finalCohort1 = newCohort1.transpose()
+finalCohort2 = newCohort2.transpose()
+finalCohort3 = newCohort3.transpose()
+finalCohort4 = newCohort4.transpose()
+finalCohort5 = newCohort5.transpose()
+finalCohort6 = newCohort6.transpose()
+finalCohort7 = newCohort7.transpose()
+finalCohort8 = newCohort8.transpose()
+finalCohortLucas = newCohortLucas.transpose()
+
+finalCohort1 = finalCohort1.sort_index()
+finalCohort2 = finalCohort2.sort_index()
+finalCohort3 = finalCohort3.sort_index()
+finalCohort4 = finalCohort4.sort_index()
+finalCohort5 = finalCohort5.sort_index()
+finalCohort6 = finalCohort6.sort_index()
+finalCohort7 = finalCohort7.sort_index()
+finalCohort8 = finalCohort8.sort_index()
+finalCohortLucas = finalCohortLucas.sort_index()
+
+with pd.ExcelWriter('set3_single285_09272020_merged.xlsx') as writer:
+    finalCohort1['MRN'].to_excel(writer, sheet_name='Cohort 1')
+    finalCohort2['MRN'].to_excel(writer, sheet_name='Cohort 2')
+    finalCohort3['MRN'].to_excel(writer, sheet_name='Cohort 3')
+    finalCohort4['MRN'].to_excel(writer, sheet_name='Cohort 4')
+    finalCohort5['MRN'].to_excel(writer, sheet_name='Cohort 5')
+    finalCohort6['MRN'].to_excel(writer, sheet_name='Cohort 6')
+    finalCohort7['MRN'].to_excel(writer, sheet_name='Cohort 7')
+    finalCohort8['MRN'].to_excel(writer, sheet_name='Cohort 8')
+
+with pd.ExcelWriter('set3_single285_lucas_09272020_merged.xlsx') as writer:
+    finalCohortLucas['MRN'].to_excel(writer, sheet_name="Lucas' set")
+
+indPos2 = pd.DataFrame(indPos)
+iaaPos2 = pd.DataFrame(iaaPos)
+
+with pd.ExcelWriter('positions.xlsx') as writer:
+    indPos2.to_excel(writer, sheet_name="Individual Positions")
+    iaaPos2.to_excel(writer, sheet_name="IAA Positions")
